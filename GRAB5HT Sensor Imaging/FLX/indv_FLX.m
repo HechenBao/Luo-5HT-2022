50344a0886a9ea9150fbfee56532a043f1c4fe2e
Hechen Bao <61467224+HechenBao@users.noreply.github.com>
2022-08-02 13:26:20 -0400

    Update indv_FLX.m

--- GRAB5HT Sensor Imaging/FLX/indv_FLX.m
+++ GRAB5HT Sensor Imaging/FLX/indv_FLX.m
@@ -1,4 +1,4 @@
-function [] = indv_FLX(path_data)  % pre-process function
+function [] = indv_FLX(path_data)  % function for loading raw data and pre-processing
 %% loading data for individual mice
 %path_data = pwd;                        % get pathway
 baseline = dir(fullfile(path_data, ['*_baseline_*.mat']));     
