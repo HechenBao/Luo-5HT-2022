5628a4c3f99f2c5918c2bfbe6da957b36de78ff5
Hechen Bao <61467224+HechenBao@users.noreply.github.com>
2022-08-02 13:29:17 -0400

    Update GRAB5HT_excecute.m

--- GRAB5HT Sensor Imaging/GRAB5HT_excecute.m
+++ GRAB5HT Sensor Imaging/GRAB5HT_excecute.m
@@ -1,9 +1,11 @@
+% Select data directory to exceute all functions and output results
+
 % %% FLX
-% GRAB5HT_FLX_bp('D:\5HT sensor\GRAB5HT photometry\A_Male\Male_FLX');
-% clc
-% clear
+GRAB5HT_FLX_bp('D:\5HT sensor\GRAB5HT photometry\A_Male\Male_FLX');
+clc
+clear
 % 
-% GRAB5HT_FLX_bp('D:\5HT sensor\GRAB5HT photometry\B_Female\Female_FLX');
+GRAB5HT_FLX_bp('D:\5HT sensor\GRAB5HT photometry\B_Female\Female_FLX');
 % clc
 % clear
 %% FLX var
@@ -17,13 +19,13 @@ clear
 
 
 % %% saline
-% GRAB5HT_saline_bp('D:\5HT sensor\GRAB5HT photometry\A_Male\Male_saline');
-% clc
-% clear
+GRAB5HT_saline_bp('D:\5HT sensor\GRAB5HT photometry\A_Male\Male_saline');
+clc
+clear
 % 
-% GRAB5HT_saline_bp('D:\5HT sensor\GRAB5HT photometry\B_Female\Female_saline');
-% clc
-% clear
+GRAB5HT_saline_bp('D:\5HT sensor\GRAB5HT photometry\B_Female\Female_saline');
+clc
+clear
 %% saline var
 GRAB5HT_saline_var('D:\5HT sensor\GRAB5HT photometry\A_Male\Male_saline');
 clc
@@ -35,13 +37,13 @@ clear
 
 
 %% Stress
-% GRAB5HT_stress_bp('D:\5HT sensor\GRAB5HT photometry\A_Male\Male_stress');
-% clc
-% clear
+GRAB5HT_stress_bp('D:\5HT sensor\GRAB5HT photometry\A_Male\Male_stress');
+clc
+clear
 % 
-% GRAB5HT_stress_bp('D:\5HT sensor\GRAB5HT photometry\B_Female\Female_stress');
-% clc
-% clear
+GRAB5HT_stress_bp('D:\5HT sensor\GRAB5HT photometry\B_Female\Female_stress');
+clc
+clear
 %% Stress var
 GRAB5HT_stress_var('D:\5HT sensor\GRAB5HT photometry\A_Male\Male_stress');
 clc
